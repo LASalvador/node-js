9ad810610fb3b4af865e14cc6644a1589dba5b0c
Lucas Salvador <lucas.salvador11358@gmail.com>
2019-06-19 19:04:59 -0300

    atualizando projeto

--- 17-templateEngines/index.js
+++ 17-templateEngines/index.js
@@ -3,13 +3,13 @@ const express = require('express');
 var app = express();
 
 app.set('view engine', 'ejs');
+app.use('/assets', express.static('assests'));
 
-
-app.get('/',function(req, res){
-	res.sendFile(__dirname + '/index.html');
+app.get('/',function(req, res, next){
+	res.render('index');
 });
 app.get('/contact', function(req,res){
-	res.sendFile(__dirname + '/contact.html');
+	res.render('contact');
 })
 app.get('/profile/:name', function(req,res){
 	var data = {age: 29, job: 'dev', hobbies:['eating', 'coding','fishing','sleeping']}
